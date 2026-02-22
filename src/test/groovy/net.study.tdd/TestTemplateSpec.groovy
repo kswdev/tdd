dbf49dba59da6f784dbdf6100852ce75ad0074f2
ksw <seonwongim@gmail.com>
2026-02-22 17:32:05 +0900

    [추가] - TestTemplateSpec.groovy
    unknownVariablesAreIgnored()
     - 사용되지 않는 템플릿 변수는 무시한다

--- src/test/groovy/net.study.tdd/TestTemplateSpec.groovy
+++ src/test/groovy/net.study.tdd/TestTemplateSpec.groovy
@@ -8,25 +8,25 @@ class TestTemplateSpec extends Specification {
     def oneVariable() {
         given:
         String name = "Reader"
-        Template template = new Template('Hello, ${name}');
+        Template template = new Template('Hello, ${name}')
 
         when:
-        template.set("name", name);
+        template.set("name", name)
 
         then:
-        "Hello, Reader" == template.evaluate();
+        "Hello, Reader" == template.evaluate()
     }
 
     def differentValue() {
         given:
         String name = "Someone else"
-        Template template = new Template('Hi, ${name}');
+        Template template = new Template('Hi, ${name}')
 
         when:
-        template.set("name", name);
+        template.set("name", name)
 
         then:
-        "Hi, Someone else" == template.evaluate();
+        "Hi, Someone else" == template.evaluate()
     }
 
     def multipleVariables() {
@@ -34,7 +34,7 @@ class TestTemplateSpec extends Specification {
         String one = "1"
         String two = "2"
         String three = "3"
-        Template template = new Template('${one}, ${two}, ${three}');
+        Template template = new Template('${one}, ${two}, ${three}')
 
         when:
         template.set("one", one)
@@ -42,6 +42,19 @@ class TestTemplateSpec extends Specification {
         template.set("three", three)
 
         then:
-        "1, 2, 3" == template.evaluate();
+        "1, 2, 3" == template.evaluate()
+    }
+
+    def unknownVariablesAreIgnored() {
+        given:
+        String name = "Reader"
+        Template template = new Template('Hello, ${name}')
+
+        when:
+        template.set("name", name)
+        template.set("doesnotexist", "Hi");
+
+        then:
+        "Hello, Reader" == template.evaluate()
     }
 }
