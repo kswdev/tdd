518c17a4705fe4a18d84af83ea3489855829f876
ksw <seonwongim@gmail.com>
2026-02-22 18:05:36 +0900

    [리팩토링] - TestTemplateSpec.groovy
    oneVariable, differentValue 테스트는
    multipleVariable 테스트와 중복되므로 삭제한다.
    
    multipleVariable. unknownVariablesAreIgnored
    테스트는 같은 인스턴스로 테스트 할 수 있으므로 미리 Template 인스턴스를 테스트 전에 만들어 둔다.

--- src/test/groovy/net.study.tdd/TestTemplateSpec.groovy
+++ src/test/groovy/net.study.tdd/TestTemplateSpec.groovy
@@ -5,56 +5,31 @@ import spock.lang.Specification
 
 class TestTemplateSpec extends Specification {
 
-    def oneVariable() {
-        given:
-        String name = "Reader"
-        Template template = new Template('Hello, ${name}')
+    private Template template
 
-        when:
-        template.set("name", name)
-
-        then:
-        "Hello, Reader" == template.evaluate()
-    }
-
-    def differentValue() {
-        given:
-        String name = "Someone else"
-        Template template = new Template('Hi, ${name}')
-
-        when:
-        template.set("name", name)
-
-        then:
-        "Hi, Someone else" == template.evaluate()
+    def setup() {
+        template = new Template('${one}, ${two}, ${three}');
+        template.set("one", "1");
+        template.set("two", "2");
+        template.set("three", "3");
     }
 
     def multipleVariables() {
-        given:
-        String one = "1"
-        String two = "2"
-        String three = "3"
-        Template template = new Template('${one}, ${two}, ${three}')
 
         when:
-        template.set("one", one)
-        template.set("two", two)
-        template.set("three", three)
+        def result = template.evaluate()
 
         then:
-        "1, 2, 3" == template.evaluate()
+        result == "1, 2, 3"
     }
 
     def unknownVariablesAreIgnored() {
-        given:
-        String name = "Reader"
-        Template template = new Template('Hello, ${name}')
 
         when:
-        template.set("name", name)
-        template.set("doesnotexist", "Hi");
+        template.set("doesnotexist", "whatever")
+        def result = template.evaluate()
 
         then:
-        "Hello, Reader" == template.evaluate()
+        result == "1, 2, 3"
     }
 }
