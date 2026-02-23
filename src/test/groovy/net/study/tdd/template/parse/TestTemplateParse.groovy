766aded0aa25e9c6c25e34e264ef11e4d6851074
ksw <seonwongim@gmail.com>
2026-02-24 02:08:18 +0900

    Refactor: 테스트 메서드 제거
    - parsingTemplateIntoStringList
      String 파싱은 사용하지 않으므로 제거

--- src/test/groovy/net/study/tdd/template/parse/TestTemplateParse.groovy
+++ src/test/groovy/net/study/tdd/template/parse/TestTemplateParse.groovy
@@ -7,24 +7,6 @@ import spock.lang.Specification
 
 class TestTemplateParse extends Specification {
 
-    def parsingTemplateIntoStringList() {
-
-        given:
-        TemplateParse parse = new TemplateParse();
-
-        when:
-        List<String> segments = parse.parse(template);
-
-        then:
-        segments == expectSegments
-
-        where:
-        scenario                    | template             | expectSegments
-        'empty template'            | ''                   | List.of('')
-        'plain text only'           | 'plain text only'    | List.of('plain text only')
-        'parsing multiple variable' | '${a} : ${b} : ${c}' | List.of('${a}', ' : ', '${b}', ' : ', '${c}')
-    }
-
     def parsingTemplateIntoSegmentList() {
 
         given:
